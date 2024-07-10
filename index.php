8f7093881ac99bf603ee15d85bef6d02bf17ae4f
Huang95 <xd9527@gmail.com>
2024-07-10 10:37:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
 <?php
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
+echo showUserIp();
+
+/**
+* 取得目前的IP位址
+*
+* @return string
+*/
+function showUserIp()
 {
-echo $_SERVER['HTTP_CLIENT_IP'];
-}
-else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-{
-echo $_SERVER['HTTP_X_FORWARDED_FOR'];
-}
-else
-{
-echo $_SERVER['REMOTE_ADDR'];
+	if (isset($_SERVER)) {
+		if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$realip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+			$realip = $_SERVER['HTTP_CLIENT_IP'];
+		} else {
+			$realip = $_SERVER['REMOTE_ADDR'];
+		}
+	} else {
+		if (getenv("HTTP_X_FORWARDED_FOR")) {
+			$realip = getenv("HTTP_X_FORWARDED_FOR");
+		} elseif (getenv("HTTP_CLIENT_IP")) {
+			$realip = getenv("HTTP_CLIENT_IP");
+		} else {
+			$realip = getenv("REMOTE_ADDR");
+		}
+	}
+	return $realip;
 }
 ?>
